b34452cb7b2863c0b310e6fc960638de202f80d4
Jonah <jonahemmanuel54@gmail.com>
2023-09-22 10:52:03 -0700

    installing a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-package { 'python3-flask':
-    ensure => '2.1.0',
-    provider => 'pip3',
+# installing flask from pip3
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
